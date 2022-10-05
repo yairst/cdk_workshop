76c04d9852eaf8f9967936e0d99a9e7bd69d6b96
yairst <yairstark@gmail.com>
2022-10-05 20:22:21 +0300

    Revert "small cahnge to lambda code"
    This reverts commit 0d5a594fcbc5ab628abf241068980b1721978e0a.

--- lambda/hello.py
+++ lambda/hello.py
@@ -7,5 +7,5 @@ def handler(event, context):
         'headers': {
             'Content-Type': 'text/plain'
         },
-        'body': 'Good Night, CDK! You have hit {}\n'.format(event['path'])
+        'body': 'Hello, CDK! You have hit {}\n'.format(event['path'])
     }
