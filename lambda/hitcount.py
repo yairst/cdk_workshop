589e2bce63e980df1ff967bc6901e2f444ab0c14
yairst <yairstark@gmail.com>
2022-10-06 10:40:50 +0300

    another little typo fix

--- lambda/hitcount.py
+++ lambda/hitcount.py
@@ -10,7 +10,7 @@ _lambda = boto3.client('lambda')
 def handler(event, context):
     print('request: {}'.format(json.dumps(event)))
     table.update_item(
-        key={'path': event['path']},
+        Key={'path': event['path']},
         UpdateExpression='ADD hits :incr',
         ExpressionAttributeValues={':incr': 1}
     )
