e3f80928cf20d7a0053e3398bc76f1af040417f3
yairst <yairstark@gmail.com>
2022-10-07 17:06:07 +0300

    update cdk deploy entrypoint

--- app.py
+++ app.py
@@ -2,10 +2,10 @@
 
 import aws_cdk as cdk
 
-from cdk_workshop.cdk_workshop_stack import CdkWorkshopStack
+from cdk_workshop.pipeline_stack import WorkshopPipelineStack
 
 
 app = cdk.App()
-CdkWorkshopStack(app, "cdk-workshop")
+WorkshopPipelineStack(app, "WorkshopPipelineStack")
 
 app.synth()
