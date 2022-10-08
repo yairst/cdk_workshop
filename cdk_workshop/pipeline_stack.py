692c324a13b7c82a79c478bd9e727f8584925a95
yairst <yairstark@gmail.com>
2022-10-08 15:14:37 +0300

    remove codeCommit from stack and define pipeline with connection to github

--- cdk_workshop/pipeline_stack.py
+++ cdk_workshop/pipeline_stack.py
@@ -1,7 +1,7 @@
 from constructs import Construct
 from aws_cdk import (
     Stack,
-    aws_codecommit as codecommit,
+    pipelines,
 )
 
 class WorkshopPipelineStack(Stack):
@@ -9,10 +9,21 @@ class WorkshopPipelineStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # Creates a CodeCommit repository called 'WorkshopRepo'
-        repo = codecommit.Repository(
-            self, 'WorkshopRepo',
-            repository_name= "WorkshopRepo"
-        )
-
         # Pipeline code goes here
+        pipeline = pipelines.CodePipeline(
+            self,
+            "Pipeline",
+            synth=pipelines.ShellStep(
+                "Synth",
+                input=pipelines.CodePipelineSource.connection(
+                    'yairst/cdk_workshop',
+                    "main",
+                    "***REMOVED***"
+                    ),
+                commands=[
+                    "npm install -g aws-cdk",  # Installs the cdk cli on Codebuild
+                    "pip install -r requirements.txt",  # Instructs Codebuild to install required packages
+                    "cdk synth",
+                ]
+            ),
+        )
