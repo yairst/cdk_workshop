0ecb3129ee76e8abfd8612f84c3de47cf3dc94dd
yairst <yairstark@gmail.com>
2022-10-08 17:14:27 +0300

    add endpoints as cloudFormation outputs

--- cdk_workshop/cdk_workshop_stack.py
+++ cdk_workshop/cdk_workshop_stack.py
@@ -1,6 +1,7 @@
 from constructs import Construct
 from aws_cdk import (
     Stack,
+    CfnOutput,
     aws_lambda as _lambda,
     aws_apigateway as apigw,
 )
@@ -10,6 +11,14 @@ from .hitcounter import HitCounter
 
 
 class CdkWorkshopStack(Stack):
+
+    @property
+    def hc_endpoint(self):
+        return self._hc_endpoint
+
+    @property
+    def hc_viewer_url(self):
+        return self._hc_viewer_url
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
@@ -26,14 +35,24 @@ class CdkWorkshopStack(Stack):
             downstream=my_lambda
         )
 
-        apigw.LambdaRestApi(
+        gateway = apigw.LambdaRestApi(
             self, 'EndPoint',
             handler=hello_with_counter._handler,
         )
 
-        TableViewer(
+        tv = TableViewer(
             self, 'viewHitCounter',
             title='Hello Hits',
             table=hello_with_counter.table,
             sort_by='-hits'
+        )
+
+        self._hc_endpoint = CfnOutput(
+            self, 'GatewayUrl',
+            value = gateway.url
+        )
+
+        self._hc_viewer_url = CfnOutput(
+            self, 'TableViewerUrl',
+            value=tv.endpoint
         )
