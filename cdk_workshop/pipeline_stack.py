1e00315a7ee6e0a6fac7c50821c283399d1fbd2c
yairst <yairstark@gmail.com>
2022-10-09 15:52:09 +0300

    read connection arn from parameter store instead from an ini file

--- cdk_workshop/pipeline_stack.py
+++ cdk_workshop/pipeline_stack.py
@@ -2,18 +2,22 @@ from constructs import Construct
 from aws_cdk import (
     Stack,
     pipelines,
+    aws_ssm as ssm,
 )
 from cdk_workshop.pipeline_stage import WorkshopPipelineStage
-from configparser import ConfigParser
+
 
 class WorkshopPipelineStack(Stack):
 
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # read config file to get the CodeStar connection ARN needed below
-        config = ConfigParser()
-        config.read('app_config.ini')
+        # get github connection arn from parameter store
+        connection_arn = ssm.StringParameter.from_string_parameter_name(
+            self,
+            "ConnectionARN",
+            "github-connection-arn"
+            ).string_value
 
         # Pipeline code goes here
         pipeline = pipelines.CodePipeline(
@@ -22,7 +26,7 @@ class WorkshopPipelineStack(Stack):
             synth=pipelines.ShellStep(
                 "Synth",
                 input=pipelines.CodePipelineSource.connection('yairst/cdk_workshop', 'main',
-                connection_arn=config['arns']['ConnectionArn']
+                connection_arn=connection_arn
                 ),
                 commands=[
                     "npm install -g aws-cdk",  # Installs the cdk cli on Codebuild
